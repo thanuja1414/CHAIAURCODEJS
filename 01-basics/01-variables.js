a48da45b4a5ef7db71410acb19faf04bf38f4e3a
thanuja1414 <thanujagatakala1414@gmail.com>
2026-09-05 23:01:49 +0530

    variables

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,7 @@
+const accountID = 14456;
+let accountEmail = "tan@gmail.com";
+var accountPassword = "tan@123";
+accountCity = "jaipur";
+let accountState;
+
+console.table([accountID,accountEmail,accountPassword,accountCity,accountState]);
---
+++ 01-basics/02-datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("hello") -> can't be run in nodejs for now , but there is a special syntax so we can use alert in local machine rather than in browser.
+
+let name = 'tanuja'
+let age = 22
+let isLoggedIn = false
+let state = null
+
+//primitive datatypes
+// number -> 2 to the power 53
+// big int
+// bool -> true / false
+// null -> represents an empty value , intentionally leaving a variable empty (not undefined) -> standalone value 
+// undefined -> variable is declared but yet not assigned with a value.
+// symbol -> used to find uniqueness
+// object
+
+console.log(typeof age)
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
+
