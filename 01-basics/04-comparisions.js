f135c156355d4266b65c73b7d358db0207c51255
thanuja1414 <thanujagatakala1414@gmail.com>
2026-09-05 23:38:06 +0530

    comparisions

---
+++ 01-basics/04-comparisions.js
@@ -0,0 +1,15 @@
+
+console.log("2">1)
+console.log("02">1)
+
+
+//avoid comparisions with null and undefined
+console.log(null > 0)
+console.log(null >= 0)
+console.log(null == 0)
+
+console.log(undefined > 0)
+console.log(undefined >= 0)
+console.log(undefined == 0)
+
+// === strict check , not only checks the value but also the datatype
